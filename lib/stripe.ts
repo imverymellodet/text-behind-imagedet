46a477814cf122bc46856de7d4d1b3282c602980
imverymellodet <mello@romellomorris.co>
2025-07-28 23:10:41 -0400

    Update stripe.ts dummy

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,9 @@
-//import Stripe from "stripe";
-
-//export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-//  apiVersion: "2024-10-28.acacia"
-//});
+// lib/stripe.ts
+export const stripe = {
+  // Dummy method to avoid runtime errors
+  checkout: {
+    sessions: {
+      create: () => ({ id: "dummy_session_id" }),
+    },
+  },
+};
